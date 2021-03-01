0c253b025939f1c6466ff9826f587a25d35f84eb
Adeleye Ayodeji <biodunhi@gmail.com>
2021-03-01 18:28:09 +0100

    Added new files

--- ade-custom-shipping.php
+++ ade-custom-shipping.php
@@ -18,8 +18,10 @@ if ( ! defined( 'ADE_CUSTOM_PLGUN_FILE' ) ) {
 }
 
 add_filter( 'woocommerce_states', 'ade_custom_shipping' );
+add_filter( 'woocommerce_states', 'ade_custom_shipping_gh' );
 
 $json = json_decode(file_get_contents(plugin_dir_path( ADE_CUSTOM_PLGUN_FILE ) . 'inc/all_cities.json'));
+$json_gh = json_decode(file_get_contents(plugin_dir_path( ADE_CUSTOM_PLGUN_FILE ) . 'inc/all_cities_gh.json'));
 
 
 function ade_custom_shipping( $states ) {
@@ -29,13 +31,33 @@ function ade_custom_shipping( $states ) {
 
     foreach ( $json as $city) {
         for ($i=0; $i < count($city->lgas); $i++) { 
-            $map[strtolower(str_replace(" ", "", $city->lgas[$i]))] = $city->lgas[$i] . ', '.$city->state;
+            $map[] = $city->lgas[$i] . ', '.$city->state;
         }
     }
-    
+
     $states['NG'] = $map;
-    
+
     return $states;
 }
 
-//Proudly made by Adeleye Ayodeji
+function ade_custom_shipping_gh( $states ) {
+    global $json_gh;
+
+    $map = array();
+
+    foreach ( $json_gh as $city) {
+        if(count($city->lgas) == 0){
+            $map[] = $city->region;
+        }else{
+            for ($i=0; $i < count($city->lgas); $i++) { 
+                $map[] = $city->lgas[$i] . ', '.$city->state. ', '.$city->region;
+            }
+        }
+    }
+
+    $states['GH'] = $map;
+
+    return $states;
+}
+
+//Proudly made by Adeleye Ayodeji => adeleyeayodeji.com 
